8236398caba4afca519fb6aa16a1186955411abd
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-07-08 23:51:09 +0545

    list all cmd commands

---
+++ src/cli.js
@@ -0,0 +1,41 @@
+const minimist = require("minimist");
+const chalk = require("chalk");
+const { help } = require("./help");
+const { filter } = require("./filter");
+const { portfolioValueAll } = require("./calculatePortfolio");
+
+const cliCommand = async () => {
+  const argsArray = process.argv;
+  const args = minimist(argsArray.slice(2));
+  let cmd = args._[0] || "help";
+
+  if (args.help || args.h) {
+    cmd = "help";
+  }
+
+  switch (cmd) {
+    case "help":
+      help(args);
+      break;
+
+    case "overview":
+      console.log(`${chalk.greenBright("Portfolio Details")}`);
+
+      const valueOfAllPortfolio = await portfolioValueAll();
+
+      console.log(`Current Balance: $${valueOfAllPortfolio.toFixed(2)}`);
+      break;
+
+    case "filter":
+      filter();
+      break;
+
+    default:
+      console.error(
+        `"${cmd}" is not a valid command!\nUse node index.js for help`
+      );
+      break;
+  }
+};
+
+exports.default = cliCommand();
