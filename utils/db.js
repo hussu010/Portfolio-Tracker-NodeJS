fb19d5886183c071c23a533e99f1e85eb3d0eb0c
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-07-08 17:25:46 +0545

    functions to filter and return transactions from db

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 const db = require("../db");
 
-const getAllTransactionsFromDB = async () => {
+const getAllTransactions = async () => {
   return new Promise((resolve, reject) => {
     db.all(`select * from transactions`, [], (err, rows) => {
       if (err) reject(err); // I assume this is how an error is thrown with your db callback
@@ -9,6 +9,47 @@ const getAllTransactionsFromDB = async () => {
   });
 };
 
+const getTransactionsByToken = async (token) => {
+  return new Promise((resolve, reject) => {
+    db.all(`select * from transactions where token=?`, [token], (err, rows) => {
+      if (err) reject(err); // I assume this is how an error is thrown with your db callback
+      resolve(rows);
+    });
+  });
+};
+
+const getTransactionsBetweenTimestamp = async (date) => {
+  const startOfTheDayInEpoch = new Date(date).getTime() / 1000;
+  const endOfTheDayInEpoch = startOfTheDayInEpoch + 86400;
+  return new Promise((resolve, reject) => {
+    db.all(
+      `select * from transactions where timestamp between ? and ?`,
+      [startOfTheDayInEpoch, endOfTheDayInEpoch],
+      (err, rows) => {
+        if (err) reject(err); // I assume this is how an error is thrown with your db callback
+        resolve(rows);
+      }
+    );
+  });
+};
+
+const getTransactionsByTokenAndDate = async (token, date) => {
+  const startOfTheDayInEpoch = new Date(date).getTime() / 1000;
+  const endOfTheDayInEpoch = startOfTheDayInEpoch + 86400;
+  return new Promise((resolve, reject) => {
+    db.all(
+      `select * from transactions where timestamp between ? and ? and token=?`,
+      [startOfTheDayInEpoch, endOfTheDayInEpoch, token],
+      (err, rows) => {
+        if (err) reject(err); // I assume this is how an error is thrown with your db callback
+        resolve(rows);
+      }
+    );
+  });
+};
 module.exports = {
-  getAllTransactionsFromDB,
+  getAllTransactions,
+  getTransactionsByToken,
+  getTransactionsBetweenTimestamp,
+  getTransactionsByTokenAndDate,
 };
