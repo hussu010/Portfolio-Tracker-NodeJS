3b2fb2b9d777db62eee3d364ed37249d3288a379
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-07-09 10:15:04 +0545

    validate if token and date exists

--- utils/commands.js
+++ utils/commands.js
@@ -2,6 +2,16 @@ const chalk = require("chalk");
 
 const validateDate = (date) => {
   var regEx = /^\d{4}-\d{2}-\d{2}$/;
+  if (typeof date !== "string") {
+    console.warn(
+      `Please use the correct date format.\nRequired format: ${chalk.red(
+        "YYYY-MM-DD"
+      )}.
+        `
+    );
+    return false;
+  }
+
   if (!date || !date.match(regEx)) {
     console.warn(
       `Please use the correct date format.\nRequired format: ${chalk.red(
@@ -15,6 +25,15 @@ const validateDate = (date) => {
 };
 
 const validateToken = (token) => {
+  if (typeof token !== "string") {
+    console.warn(
+      `You will need to pass token symbol..\neg: ${chalk.greenBright(
+        "node index.js filter -t=BTC"
+      )}`
+    );
+    return false;
+  }
+
   if (!token) {
     console.warn(
       `You will need to pass token symbol..\neg: ${chalk.greenBright(
