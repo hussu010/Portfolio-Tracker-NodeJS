18906f67dc80a9a79790210d05b8609be2f99a67
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-07-09 12:03:25 +0545

    include the separator tag for csv import

--- syncDB.sql
+++ syncDB.sql
@@ -1,16 +1,10 @@
 select 'Dropping the transactions table if it exists already' AS '';
 DROP TABLE IF EXISTS transactions;
-
-CREATE TABLE IF NOT EXISTS transactions
-  (
-    timestamp        INT,
-    transaction_type VARCHAR(10),
-    token            VARCHAR(10),
-    amount           REAL
-);
-select 're-created the transactions table' AS '';
+CREATE TABLE IF NOT EXISTS transactions (timestamp INT, transaction_type varchar, token varchar, amount real);
 
 select 'Syncing the contents from transactions.csv to db' AS '';
-select 'Buckle up; it might take a while' AS '';
-.import --csv --skip 1 ./transactions.csv transactions
+select 'Buckle up; it might take a while' AS ''; 
+.separator ","
+.import transactions_copy.csv transactions
+delete from transactions where rowid IN (select rowid from transactions limit 1);
 select 'Yaay, the sync is complete' AS '';
