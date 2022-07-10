62f8e3cb9fb5113995feeeb4fb482095544a39a5
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-07-10 13:40:53 +0545

    return token name and amount between timestamp

--- utils/fetchTokenAndAmount.js
+++ utils/fetchTokenAndAmount.js
@@ -57,7 +57,44 @@ const getTransactionsByToken = (token) => {
   });
 };
 
+const getTransactionsBetweenTimestamp = (date) => {
+  const readTransactionsStream = fs.createReadStream(
+    path.resolve(__dirname, "../transactions_copy.csv")
+  );
+
+  const startOfTheDayInEpoch = new Date(date).getTime() / 1000;
+  const endOfTheDayInEpoch = startOfTheDayInEpoch + 86400;
+
+  var tokenNameAmount = {};
+  var allTokenNameAmount = {};
+
+  return new Promise((resolve, reject) => {
+    readTransactionsStream
+      .pipe(parse({ delimiter: ",", from_line: 2 }))
+      .on("data", (data) => {
+        const timestamp = data[0];
+        if (timestamp <= endOfTheDayInEpoch) {
+          allTokenNameAmount = calculateTokenAmountFromTransactions(
+            tokenNameAmount,
+            data
+          );
+        }
+        if (timestamp <= startOfTheDayInEpoch) {
+          resolve(allTokenNameAmount);
+          readTransactionsStream.destroy();
+        }
+      })
+      .on("end", () => {
+        resolve(allTokenNameAmount);
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+};
+
 module.exports = {
   getAllTransactions,
   getTransactionsByToken,
+  getTransactionsBetweenTimestamp,
 };
